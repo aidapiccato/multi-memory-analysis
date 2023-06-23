8ed862f595965fece051e4bbc4adc63d2ea31cbf
Isabella <isabella@dhcp-10-29-42-239.dyn.mit.edu>
2023-06-23 17:19:20 -0400

    updating models

--- utils/modeling.py
+++ utils/modeling.py
@@ -3,20 +3,27 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def generate_stim():
-    # return np.random.uniform(0,1)
-    return 0.488838
+def generate_stim(set_size):
+    stimulus = []
+    for stim in range(0,set_size):
+        stimulus.append(np.random.uniform(0,1))
+    
+    return stimulus
+
 def generate_mean(set_size):
     """generate a mean value based on the set size
 
     Args:
-        set_size (int): the amount of objects in the set
+        set_size (int[]): the amount of objects in the set
 
     Returns:
         _float_: _the calculated mean value_
     """
     mean = 7 / set_size
     return mean
+
+def cue(stim):
+    return np.random.choice(stim)
 
 def sample(mean, std, size):
     """creates a sample set
@@ -31,8 +38,8 @@ def sample(mean, std, size):
     """
     return np.random.normal(mean, std, size)
 
-def decision(stim, sample, threshold):
-    """Makes a decision about the sample data based on the threshold parameter
+def decision_random(stim, sample, cue, threshold):
+    """Makes a decision about the sample data based on the threshold parameter. This function accounts for random guessing 
 
     Args:
         samples (_float_): sample set
@@ -41,14 +48,45 @@ def decision(stim, sample, threshold):
     Returns:
         _float_: array of decisions based on the threshold
     """
-    decision = 0
 
     if sample >= threshold:
-        return stim 
+        return np.random.normal(cue,1) 
     if sample < threshold:
         return np.random.uniform(0,1)
+    
+def decision_confused(stim, sample, cue, threshold):
+    random_cue = np.random.choice(stim)
 
-def accuracy(stim, decision):
+    if sample >= threshold:
+        return np.random.normal(cue,1) 
+    if sample < threshold:
+        return np.random.normal(random_cue,1)
+    
+def find_stim_choice(stim, decision):
+    stim.sort()
+    midpoints = [[]]
+    i=0
+    if i != 0 and i != len(stim) -1:
+        while i < stim.len()-1:
+            upper_limit = (stim[i+1] - stim[i]) / 2
+            lower_limit = (stim[i] - stim[i-1]) / 2
+            midpoints.append([upper_limit,lower_limit])
+    elif i == 0:
+        upper_limit = (stim[len(stim)-1] - stim[0]) / 2
+        lower_limit = (stim[0] - stim[1]) / 2
+        midpoints.append([upper_limit,lower_limit])
+    elif i == stim.len():
+        upper_limit = (stim[len(stim)+1] - stim[len(stim)]) / 2
+        lower_limit = (stim[len(stim)] - stim[len(stim)-1]) / 2
+        midpoints.append([upper_limit,lower_limit])
+        
+    for range in midpoints:
+        if decision in range(range[0],range[1]):
+            return midpoints.index(range)
+        else:
+            return None
+
+def accuracy(cue, choice):
     """measure the accuracy of the model with the ground truth data set and the responses from the model
 
     Args:
@@ -59,9 +97,9 @@ def accuracy(stim, decision):
         _float_: accuracy and mean accuracy of comparison
     """
     
-    if stim == decision:
+    if cue == choice:
         accuracy=1
-    if stim != decision:
+    if cue != choice:
         accuracy=0
 
     return accuracy
